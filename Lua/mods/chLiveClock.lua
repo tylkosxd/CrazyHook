ab74d953e4d50f3ed51bf2d6db75360133e689c7
tylkosxd <163313185+tylkosxd@users.noreply.github.com>
2024-05-18 17:29:21 +0200

    Added chLiveClock.lua
    Implementation of MPSPEEDRUN - Real time in-game timer

---
+++ Lua/mods/chLiveClock.lua
@@ -0,0 +1,58 @@
+local socket = require 'socket.socket'
+
+local _live = {}
+
+local function _format_ptime(t)
+    if #tostring(t) == 1 then
+        return "0" .. t
+    else
+        return tostring(t)
+    end
+end
+
+local function _format_time(t)
+    local _msPart = string.sub(tostring(t), -3)
+    local _secPartFull = tonumber(tostring(t):match'(.*)%..*')
+    local _secPart = _format_ptime(_secPartFull%60)
+    local _minPartFull = math.floor(_secPartFull/60)
+    local _minPart = _format_ptime(_minPartFull%60)
+    local _hourPart = math.floor(_minPartFull/60)
+    return _hourPart .. ":" .. _minPart .. ":" .. _secPart .. _msPart
+end
+
+_live.clock = function(ptr)
+    if _chameleon[0] == chamStates.LoadingStart then
+        _lClockStart = nil
+        _lClockStop = nil
+    elseif _chameleon[0] == chamStates.LoadingEnd then
+        --ffi.C.PostMessageA(nRes(1,1), 0x111, 772, 0)--
+        if not _lClockStart then
+            _lClockStart = socket.gettime()
+        end
+    elseif _chameleon[0] == chamStates.Gameplay then
+        if InfosDisplay[0].LiveClock == true then
+		    if ptr then 
+			    hdc = tonumber(ffi.cast("int",ptr)) 
+		    end
+		    ffi.C.SetTextColor(hdc, 0xFFFFFF)
+            if _lClockStart then
+		        _lTime = tostring(socket.gettime() - _lClockStart):match'(%d*%.%d%d)'
+                if _lTime then
+                    if not _lClockStop then 
+		                _lClock = _format_time(_lTime)
+                    else
+                        _lClock = _format_time(_lClockStop)
+                    end
+                end
+            end
+            local screenx, screeny = nRes(33), nRes(34)
+            if _lClock then
+                local rect = ffi.new("Rect",{screenx-8-#_lClock*10, screeny-28, screenx-8, screeny-4})
+		        local lprect = ffi.new("Rect[1]", rect)
+		        ffi.C.DrawTextA(hdc, _lClock ,#_lClock, lprect, 2)
+            end
+        end
+    end
+end
+
+return _live
