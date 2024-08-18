aa0bdabfcf4ff06eeb47f7fc89409eda3a96777c
tylkosxd <163313185+tylkosxd@users.noreply.github.com>
2024-08-18 21:13:03 +0200

    Update _HD.lua

--- Lua/Assets/GAME/LOGICS/_HD.lua
+++ Lua/Assets/GAME/LOGICS/_HD.lua
@@ -1,19 +1,18 @@
 
 function main(self)
 
-	PLAY_AREA = ffi.cast("Rect*", 0x535840)[0]
+	local PLAY_AREA = mdl_exe.PlayAreaRect[0]
 
 	if self.State == 0 then
 		self.Flags.AlwaysActive, self.DrawFlags.NoDraw = true, true
-        ffi.cast("char*",0x429BAF)[0] = 1 -- lock + on keyboard
-        ffi.cast("char*",0x429B6F)[0] = 10 -- lock - on keyboard
+        PrivateCast(1, "char*", 0x429BAF) -- lock + key 
+        PrivateCast(10, "char*", 0x429B6F) -- lock - key 
 		self.State = 2
 	end
 
 	if self.State == 2 then
         if nRes(26) ~= 10 then
-            --ffi.cast("void(*__thiscall)(void*,int,int,int)", 0x429BF0)(_nResult[0],10,1,0)
-            --ffi.cast("char*",0x429BAF)[0] = 1
+            ffi.cast("void(*__thiscall)(void*,int,int,int)", 0x429BF0)(_nResult[0],10,1,0)
         end
 		if PLAY_AREA.Right + 1 ~= self.Width then
 			self.State = 3
@@ -27,8 +26,8 @@ function main(self)
 
     if self.State == 4 then
         ChangeResolution(640,480)
-        ffi.cast("char*",0x429BAF)[0] = 10  -- unlock + on keyboard
-        ffi.cast("char*",0x429B6F)[0] = 1 -- unlock - on keyboard
+        ffi.cast("char*",0x429BAF)[0] = 10  -- unlock + key
+        ffi.cast("char*",0x429B6F)[0] = 1 -- unlock - key
         self:Destroy()
     end
 end
