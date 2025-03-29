f2dcf4e5568b9904e9e171e53b63e79473799b8b
tylkosxd <163313185+tylkosxd@users.noreply.github.com>
2025-03-29 20:07:50 +0100

    Update and rename _ClawCPowerup.lua to _CustomPowerupHandler.lua

--- Lua/Assets/GAME/LOGICS/_CustomPowerupHandler.lua
+++ Lua/Assets/GAME/LOGICS/_CustomPowerupHandler.lua
@@ -1,6 +1,7 @@
 function main(object)
 	object.Flags.AlwaysActive = true
-	if _CurrentPowerup[0] ~= 666 or _PowerupTime[0] == 0 or GetClaw().Health <= 0 then
+	local claw = GetClaw()
+	if _CurrentPowerup[0] ~= 666 or _PowerupTime[0] == 0 or claw.Health <= 0 then
 		object:Destroy()
 	end
 	if type(object.CPN) == "function" then
