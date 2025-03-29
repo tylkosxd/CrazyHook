072d79345f5654e2fd27b1c701d1399fd798ee28
tylkosxd <163313185+tylkosxd@users.noreply.github.com>
2025-03-29 20:09:32 +0100

    Update _TestLogic.lua

--- Lua/Assets/GAME/LOGICS/_TestLogic.lua
+++ Lua/Assets/GAME/LOGICS/_TestLogic.lua
@@ -1,27 +1,36 @@
 
+local switch = {
+    [0] = function(self)
+        ActivateDebugText()
+        snRes(1,18,74)
+        self.testNumber = 1
+        self.Flags.AlwaysActive = true
+        self.checker = CreateObject{Image = "GAME_SOUNDICON", MoveRect = {-10, -10, 10, 10}, Logic = "DoNothing", Z=9000, PhysicsType = 8}
+        if not self.path then
+            self.path = "Assets\\GAME\\LOGICS\\Test\\Test.lua"
+        end
+        self.State = 1
+    end,
+    [1] = function(self)
+        self.State = KeyPressed"F5" and 2 or 1
+    end,
+    [2] = function(self)
+        local testNb = self.testNumber
+        local test, err = loadfile(self.path)
+        assert(test, err)
+        local temp = setmetatable({}, {__index = _G})
+        setfenv(test, temp)
+        test()
+        temp["test"](self, testNb)
+        self.testNumber = testNb + 1
+        self.State = 3
+    end,
+    [3] = function(self)
+        self.State = not KeyPressed"F5" and 1 or 3
+    end
+}
+
 function main(self)
-	if self.State == 0 then
-		self.Flags.AlwaysActive, self.DrawFlags.NoDraw = true, true
-		self.count = 0
-		self.State = 1
-	end
-
-	if self.State == 1 and GetInput"Special" then
-        self.State = 2
-	end
-
-	if self.State == 2 and not GetInput"Special" then
-
-		self.State = 3
-	end
-	
-	if self.State == 3 and KeyPressed"5" then
-		GetTime = function() return 100 end
-		MessageBox(version)
-		self.State = 2
-	end
-	
-	if self.State > 0 then
-		
-	end
+    switch[self.State](self)
+    debug_text[1] = "Test number: " .. self.testNumber
 end
