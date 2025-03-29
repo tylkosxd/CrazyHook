5a850ff07e7ecf0fae8429ab8523500fc88c78ad
tylkosxd <163313185+tylkosxd@users.noreply.github.com>
2025-03-29 19:44:17 +0100

    Update CrazyHook.cpp
    Refactored

--- CrazyHook/CrazyHook/CrazyHook.cpp
+++ CrazyHook/CrazyHook/CrazyHook.cpp
@@ -11,16 +11,11 @@ using namespace std;
 typedef int (*Logic)(ObjectA*);
 extern lua_State *L;
 
-static void msgbox(const char *fnName, ObjectA *a) {
-	stringstream ss;
-	ss << fnName << " " << a->_Name;
-	MessageBox(0, ss.str().c_str(), "native exception", 0);
-}
 
 void call_lua(const char* fnName, ObjectA *a) {
-	lua_getglobal(L, "_lua");
+	lua_getglobal(L, "_ch_link");
     if (!lua_isfunction(L, -1)) {
-        MessageBox(0, "Critical error: function _lua does not exit" , fnName, 0);
+        MessageBox(0, "Critical error: function _ch_link does not exit" , fnName, 0);
 		ExitProcess(1);
     }
 	lua_pushstring(L, fnName);
@@ -28,21 +23,6 @@ void call_lua(const char* fnName, ObjectA *a) {
 	__try {
 		lua_call(L, 2, 0);
 	} __except(1) {
-		msgbox(fnName, a);
-	}
-}
-
-void call_lua(const char* fnName) {
-	lua_getglobal(L, "_lua");
-	if (!lua_isfunction(L, -1)) {
-		MessageBox(0, "Critical error: function _lua does not exit", fnName, 0);
-		ExitProcess(1);
-	}
-	lua_pushstring(L, fnName);
-	__try {
-		lua_call(L, 1, 0);
-	}
-	__except (1) {
 		MessageBox(0, fnName, "native exception", 0);
 	}
 }
@@ -52,30 +32,30 @@ int CustomLogic(ObjectA* a) {
 	if(!a->v->userdata) {
 		if(a->v->state != 0) MessageBox(0, "state != 0", 0, 0);
 		userdata = new Object(a);
-		call_lua("_create", a);
+		call_lua("_ch_create", a);
 	}
 	assert(a->v->userdata->a == a);
-	call_lua("_logic", a);
+	call_lua("_ch_logic", a);
 	return 1;
 }
 
 int CustomHit(ObjectA *a) {
-	call_lua("_hit", a);
+	call_lua("_ch_hit", a);
 	return 1;
 }
 
 int CustomAttack(ObjectA *a) {
-	call_lua("_attack", a);
+	call_lua("_ch_attack", a);
 	return 1;
 }
 
 int CustomBump(ObjectA *a) {
-	call_lua("_bump", a);
+	call_lua("_ch_bump", a);
 	return 1;
 }
 
 int MenuHook(ObjectA* a) {
-	call_lua("_menu");
+	call_lua("_ch_menu", a);
 	a->v->logic = 0x4614D0; //MenuClaw
 	return 1;
 }
@@ -83,32 +63,28 @@ int MenuHook(ObjectA* a) {
 #define REGISTER_LOGIC(logic) (s->*registerLogic)(logic, #logic, 0)
 
 extern "C" {
-
-// on map load
-CRAZYHOOK_API void Chameleon(void *ptr)
-{
-	call_lua("_map", (ObjectA*)ptr);
-}
-
-// on logics register
-CRAZYHOOK_API void CrazyHook(UnknownStruct *s)
-{
-	// Step 1. Register logics
-	typedef void (UnknownStruct::*RegisterLogic)(Logic, const char*, int);
-	static_assert(sizeof(RegisterLogic) == sizeof(int), "Wrong size of function pointer");
-	RegisterLogic registerLogic;
-	(int&)registerLogic = 0x004D6F70;
-	REGISTER_LOGIC(CustomLogic);
-	REGISTER_LOGIC(CustomHit);
-	REGISTER_LOGIC(CustomAttack);
-	REGISTER_LOGIC(CustomBump);
-	REGISTER_LOGIC(MenuHook);
-
-	// Step 2. Execute CrazyHook.lua
-	if(luaL_dofile(L, "CrazyHook.lua")) {
-		MessageBox(0, lua_tostring(L, -1), "CrazyHook.lua error", 0);
-		ExitProcess(1);
+	// on map load
+	CRAZYHOOK_API void Chameleon(void *ptr) {
+		call_lua("_ch_map", (ObjectA*)ptr);
 	}
-}
 
+	// on logics register
+	CRAZYHOOK_API void CrazyHook(UnknownStruct *s) {
+		// Step 1. Register logics
+		typedef void (UnknownStruct::*RegisterLogic)(Logic, const char*, int);
+		static_assert(sizeof(RegisterLogic) == sizeof(int), "Wrong size of function pointer");
+		RegisterLogic registerLogic;
+		(int&)registerLogic = 0x004D6F70;
+		REGISTER_LOGIC(CustomLogic);
+		REGISTER_LOGIC(CustomHit);
+		REGISTER_LOGIC(CustomAttack);
+		REGISTER_LOGIC(CustomBump);
+		REGISTER_LOGIC(MenuHook);
+
+		// Step 2. Execute CrazyHook.lua
+		if(luaL_dofile(L, "CrazyHook.lua")) {
+			MessageBox(0, lua_tostring(L, -1), "CrazyHook.lua error", 0);
+			ExitProcess(1);
+		}
+	}
 }
