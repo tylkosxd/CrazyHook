87685d2a13376f9955f3de73bb5b04a35014f24b
tylkosxd <163313185+tylkosxd@users.noreply.github.com>
2025-03-29 20:05:31 +0100

    Update _PlasmaSword.lua

--- Lua/Assets/GAME/LOGICS/_PlasmaSword.lua
+++ Lua/Assets/GAME/LOGICS/_PlasmaSword.lua
@@ -1,7 +1,7 @@
 
 function main(self)
 	if self.State == 0 then
-        self.AttackTypeFlags = 2
+        self.AttackFlags.Player = true
         self.AttackRect = {-28, -36, 27, 27}
 		self:SetImage"GAME_POWERUPS_NEWPLASMASWORD"
 		self:SetAnimation"GAME_CYCLE50"
@@ -12,7 +12,8 @@ function main(self)
 			self.Smarts = self.Smarts / 1000
 		end
         self.State = 5
-    elseif self.State == 5 then
+	end
+    if self.State == 5 then
         self:AnimationStep()
 	end
 end
